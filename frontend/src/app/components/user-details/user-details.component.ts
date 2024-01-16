15c9ee9cb410222fe59e6955050aeaa9d29d76bf
Łukasz Wodniak <gerdzio53@gmail.com>
2024-01-16 22:58:08 +0100

    Adding user panel and making some fixes

--- frontend/src/app/components/user-details/user-details.component.ts
+++ frontend/src/app/components/user-details/user-details.component.ts
@@ -3,7 +3,8 @@ import {CommonModule} from '@angular/common';
 import {User} from "../../services/auth-utils";
 import {AuthenticationService} from "../../services/authentication.service";
 import {HttpClient, HttpParams} from "@angular/common/http";
-import {BASE_URL, USER_STATS} from "../../ApiCalls";
+import {BASE_URL, HARVEST, PLANTATION, USER_STATS} from "../../ApiCalls";
+import {catchError, map} from "rxjs";
 
 @Component({
   selector: 'app-user-details',
@@ -17,14 +18,16 @@ export class UserDetailsComponent implements OnInit {
   public readonly user: Signal<User>;
   public workedInPlantations: Array<any> = [];
   public futureHarvests: Array<any> = [];
-  public stats: Array<any>;
+  public stats: Array<any> = [];
 
   constructor(private _loginService: AuthenticationService, private _httpClient: HttpClient) {
     this.user = this._loginService.user;
   }
 
   ngOnInit(): void {
-    this.getStats()
+    this.getStats();
+    this.getFutureHarvests();
+    this.getWorkedInPlantations();
   }
 
   private getStats(): void {
@@ -36,11 +39,42 @@ export class UserDetailsComponent implements OnInit {
       "sortDirection": "ASC"
     }, {params: params}).subscribe({
       next: response => {
-        // @ts-ignore
-        this.stats = response.data
+        console.log(response)
+
       },
       error: err => console.error(err)
     })
+  }
+
+  private getWorkedInPlantations(): void {
+    this._httpClient.post(BASE_URL + PLANTATION + '', {})
+  }
+
+  private getFutureHarvests(): void {
+    this._httpClient.post(BASE_URL + HARVEST + '/get-future-harvests', {}).subscribe({
+      next: response => {
+        // console.log(response);
+        const harvests = response as any[];
+        harvests.forEach(harvest => {
+          // this.getPlantationName(harvest.id).subscribe(plantationName => {
+          //   console.log(plantationName)
+          // })
+          console.log(harvest)
+        })
+      },
+      error: err => console.error(err)
+    })
+  }
+
+  private getPlantationName(userHarvestId: number): any {
+    const params = new HttpParams().set('id', userHarvestId)
+    return this._httpClient.post(BASE_URL + PLANTATION + '/get-plantation-by-id', {}, {params: params}).pipe(
+      map(response => response['plantationName']),
+      catchError(error => {
+        console.error(error);
+        throw error;  // Rzucanie błędu w przypadku niepowodzenia
+      })
+    );
   }
 
 }
